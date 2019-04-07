3f579bde5c466d9e849b0017def2138896adea5a
animx11 <animx11@gmail.com>
2019-04-08 01:36:08 +0200

    Initialization and configuration backend project.

---
+++ src/main/java/MusicCube/Main.java
@@ -0,0 +1,22 @@
+package MusicCube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories("MusicCube.Repositories")
+public class Main extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Main.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
