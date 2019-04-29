6ff471165827877eaecc8b0db11617836abc40c0
animx11 <animx11@gmail.com>
2019-04-30 01:44:04 +0200

    Disabling spring security for merge, until it will be ready for work with others components

--- src/main/java/MusicCube/SecurityConfig.java
+++ src/main/java/MusicCube/SecurityConfig.java
@@ -1,22 +1,12 @@
 package MusicCube;
 
-import com.google.common.collect.ImmutableList;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig
+        //extends WebSecurityConfigurerAdapter
+{
 /*
     @Override
     public void configure(final AuthenticationManagerBuilder auth)
@@ -26,16 +16,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .password("user").roles("USER");
     }
 */
-    @Bean
-    public UserDetailsService userDetailsService(){
-        UserDetails userDetails = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
-                .roles("USER")
-                .build();
-        return new InMemoryUserDetailsManager(userDetails);
-    }
-
+/*
     @Override
     protected void configure(final HttpSecurity http) throws Exception
     {
@@ -43,13 +24,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.authorizeRequests()
                 .antMatchers("/signIn").permitAll()
-                .antMatchers("/**").hasRole("USER")
-                // equivalent to <http auto-config="true">
-                .and().formLogin()
-                .and().httpBasic()
-                .and().logout()
-                // CSRF is enabled by default (will discuss later)
-                .and().csrf().disable();
+            .antMatchers("/**").access("hasRole('USER')")    // equivalent to <http auto-config="true">
+            .and().formLogin()
+            .and().httpBasic()
+            .and().logout()
+// CSRF is enabled by default (will discuss later)
+            .and().csrf().disable();
 
 
     }
@@ -70,5 +50,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
-
+*/
 }
