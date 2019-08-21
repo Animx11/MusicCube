b5f6b1553b11176cb7bf579477b089d9b3b7f81e
animx11 <animx11@gmail.com>
2019-08-21 04:08:28 +0200

    Little changes in admin panel

--- src/app/Components/User/admin-panel/admin-panel.component.ts
+++ src/app/Components/User/admin-panel/admin-panel.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { this_url } from 'src/app/Services/API_URL';
+
+const thisUrl = this_url;
 
 @Component({
   selector: 'app-admin-panel',
@@ -7,47 +10,174 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminPanelComponent implements OnInit {
 
-  isSongClicked: boolean;
-  isBandClicked: boolean;
-  isAlbumClicked: boolean;
-  isArtistClicked: boolean;
-  isConcertClicked: boolean;
-  isGenreClicked: boolean;
-  isInstrumentClicked: boolean;
-  isCityClicked: boolean;
+  // Main
+
+  isBackClicked: boolean;
+  isShowRecordClicked: boolean;
+  isManageUsersClicked: boolean;
   showHead: boolean;
+
+  // Song
+
+  isSongClicked: boolean;
+  isSongAddClicked: boolean;
+  isSongEditClicked: boolean;
+
+  // Band
+
+  isBandClicked: boolean;
+  isBandAddClicked: boolean;
+  isBandEditClicked: boolean;
+
+  // Album
+
+  isAlbumClicked: boolean;
+  isAlbumAddClicked: boolean;
+  isAlbumEditClicked: boolean;
+
+  // Person
+
+  isPersonClicked: boolean;
+  isPersonAddClicked: boolean;
+  isPersonEditClicked: boolean;
+
+  // Concert
+
+  isConcertClicked: boolean;
+  isConcertAddClicked: boolean;
+  isConcertEditClicked: boolean;
+
+  // Genre
+
+  isGenreClicked: boolean;
+  isGenreAddClicked: boolean;
+  isGenreEditClicked: boolean;
+
+  // Instrument
+
+  isInstrumentClicked: boolean;
+  isInstrumentAddClicked: boolean;
+  isInstrumentEditClicked: boolean;
+
+  // City
+
+  isCityClicked: boolean;
+  isCityAddClicked: boolean;
+  isCityEditClicked: boolean;
+
+
 
   constructor() { }
 
   ngOnInit() {
+
+    this.isBackClicked = false;
+    this.isShowRecordClicked = false;
+    this.isManageUsersClicked = false;
+    this.showHead = false;
+ 
+    this.isSongClicked = false;
+    this.isSongAddClicked = false;
+    this.isSongEditClicked = false;
+
+    this.isBandClicked = false;
+    this.isBandAddClicked = false;
+    this.isBandEditClicked = false;
+
+    this.isAlbumClicked = false;
+    this.isAlbumAddClicked = false;
+    this.isAlbumEditClicked = false;
+
+    this.isPersonClicked = false;
+    this.isPersonAddClicked = false;
+    this.isPersonEditClicked = false;
+
+    this.isConcertClicked = false;
+    this.isConcertAddClicked = false;
+    this.isConcertEditClicked = false;
+
+    this.isGenreClicked = false;
+    this.isGenreAddClicked = false;
+    this.isGenreEditClicked = false;
+
+    this.isInstrumentClicked = false;
+    this.isInstrumentAddClicked = false;
+    this.isInstrumentEditClicked = false;
+
+    this.isCityClicked = false;
+    this.isCityAddClicked = false;
+    this.isCityEditClicked = false;
+
+  }
+
+  // Resets
+
+  resetMainValue() {
+    this.isBackClicked = false;
+    this.isShowRecordClicked = false;
+    this.isManageUsersClicked = false;
+    this.showHead = false;
+  }  
+
+  resetRecordValue() {
     this.isSongClicked = false;
     this.isBandClicked = false;
     this.isAlbumClicked  = false;
-    this.isArtistClicked = false;
+    this.isPersonClicked = false;
     this.isConcertClicked = false;
     this.isGenreClicked = false;
     this.isInstrumentClicked = false;
     this.isCityClicked = false;
-    this.showHead = false;
+  }
+
+  resetOptionValue() {
+    this.isSongAddClicked = false;
+    this.isSongEditClicked = false;
+    this.isBandAddClicked = false;
+    this.isBandEditClicked = false;
+    this.isAlbumAddClicked = false;
+    this.isAlbumEditClicked = false;
+    this.isPersonAddClicked = false;
+    this.isPersonEditClicked = false;
+    this.isConcertAddClicked = false;
+    this.isConcertEditClicked = false;
+    this.isGenreAddClicked = false;
+    this.isGenreEditClicked = false;
+    this.isInstrumentAddClicked = false;
+    this.isInstrumentEditClicked = false;
+    this.isCityAddClicked = false;
+    this.isCityEditClicked = false;
+  }
+
+  resetValues() {
+    this.resetMainValue();
+    this.resetRecordValue();
+    this.resetOptionValue();
+  }
+
+  // Switch
+
+  swichMainValue(clicked: boolean): boolean {
+    return !clicked;
   }
 
 
-  switchValue(clicked: boolean): boolean {
-    this.resetValue();
+  switchRecordValue(clicked: boolean): boolean {
+    this.resetOptionValue();
+    this.resetRecordValue();
+    clicked = false;
     this.showHead = true;
     return !clicked;
   }
 
-  resetValue() {
-    this.isSongClicked = false;
-    this.isBandClicked = false;
-    this.isAlbumClicked  = false;
-    this.isArtistClicked = false;
-    this.isConcertClicked = false;
-    this.isGenreClicked = false;
-    this.isInstrumentClicked = false;
-    this.isCityClicked = false;
-    this.showHead = false;
+  switchOptionValue(clicked: boolean): boolean {
+    this.resetOptionValue();
+    clicked = false;
+    return !clicked;
+  }
+
+  userPanel() {
+    window.location.replace(thisUrl + "/UserPanel")
   }
 
 }
