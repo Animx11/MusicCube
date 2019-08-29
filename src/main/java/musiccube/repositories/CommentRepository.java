9a1fbccd95ec6e73f1612478532bac50cbe48b4f
animx11 <animx11@gmail.com>
2019-08-29 21:54:22 +0200

    Added comment repository

---
+++ src/main/java/musiccube/repositories/CommentRepository.java
@@ -0,0 +1,15 @@
+package musiccube.repositories;
+
+import musiccube.entities.Comment;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.Optional;
+
+public interface CommentRepository extends CrudRepository<Comment, Integer>, PagingAndSortingRepository<Comment, Integer> {
+
+    @Query("SELECT c FROM Comment c WHERE c.song.id = ?1")
+    Iterable<Comment> findCommentsBySongId(int id);
+
+}
