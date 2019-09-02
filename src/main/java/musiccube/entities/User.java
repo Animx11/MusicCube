9e7b1344b4a7b20f0a96123e4643f7e15d48ad63
przand1 <przand1@st.amu.edu.pl>
2019-09-03 01:31:45 +0200

    x

--- src/main/java/musiccube/entities/User.java
+++ src/main/java/musiccube/entities/User.java
@@ -14,7 +14,7 @@ import java.util.Set;
 @Table(
         name = "SystemUsers",
         uniqueConstraints = {
-            @UniqueConstraint(columnNames = {"user_id"})
+            @UniqueConstraint(columnNames = {"id"})
         }
 )
 public class User {
