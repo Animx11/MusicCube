f77537dd6377a858871d65baaa10d98ab2b0a05e
przand1 <przand1@st.amu.edu.pl>
2019-09-03 03:01:49 +0200

    update url

--- src/app/Utils/API_URL.ts
+++ src/app/Utils/API_URL.ts
@@ -1,5 +1,5 @@
 // export const api_url = "http://192.168.1.112:8080/api";
 
-export const api_url = 'http://192.168.1.66:8080/api';
+export const api_url = 'https://musiccube-server.herokuapp.com/api';
 export const this_url = 'http://localhost:4200/';
 
