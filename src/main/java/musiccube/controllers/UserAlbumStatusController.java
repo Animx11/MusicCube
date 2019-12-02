616f9cb735bf52051891a8f3e297e1ac5fd83773
przand1 <przand1@st.amu.edu.pl>
2019-12-02 15:13:00 +0100

    GET methods for extras controllers

--- src/main/java/musiccube/controllers/UserAlbumStatusController.java
+++ src/main/java/musiccube/controllers/UserAlbumStatusController.java
@@ -1,11 +1,31 @@
 package musiccube.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import musiccube.entities.UserAlbumStatus;
+import musiccube.services.useralbumstatus.UserAlbumStatusService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 @CrossOrigin(origins = "${serverAddress}")
 public class UserAlbumStatusController {
+    @Autowired
+    private UserAlbumStatusService statusService;
+
+    @GetMapping (
+            path = "/user/owned-albums/{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Iterable<UserAlbumStatus> getOwnedAlbums(@PathVariable(name = "id") int userId) {
+        return statusService.getOwnedByUserId(userId);
+    }
+
+    @GetMapping (
+            path = "/user/sought-albums/{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Iterable<UserAlbumStatus> getSoughtAlbums(@PathVariable(name = "id") int userId) {
+        return statusService.getSoughtByUserId(userId);
+    }
 }
--- src/main/java/musiccube/controllers/UserSongStatusController.java
+++ src/main/java/musiccube/controllers/UserSongStatusController.java
@@ -1,11 +1,32 @@
 package musiccube.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import musiccube.entities.UserSongStatus;
+import musiccube.services.usersongstatus.UserSongStatusService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 @CrossOrigin(origins = "${serverAddress}")
 public class UserSongStatusController {
+    @Autowired
+    private UserSongStatusService statusService;
+
+
+    @GetMapping(
+            path = "/user/listened-songs/{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Iterable<UserSongStatus> getListenedSongs(@PathVariable(name = "id") int userId) {
+        return statusService.getListenedByUserId(userId);
+    }
+    @GetMapping(
+            path = "/user/songs-to-listen/{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Iterable<UserSongStatus> getSongsToListen(@PathVariable(name = "id") int userId) {
+        return statusService.getToListenByUserId(userId);
+
+    }
 }
