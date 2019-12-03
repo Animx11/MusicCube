c1a8ff793e668298c0537eb67f356406e6de1d0b
przand1 <przand1@st.amu.edu.pl>
2019-12-03 13:26:17 +0100

    add status classes

--- src/app/Components/User/user-panel/extras/extras.component.ts
+++ src/app/Components/User/user-panel/extras/extras.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {UserSongStatus} from '../../../../Class/UserSongStatus';
+import {UserAlbumStatus} from '../../../../Class/UserAlbumStatus';
 
 @Component({
   selector: 'app-extras',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExtrasComponent implements OnInit {
 
+  isYourSongsClicked: boolean;
+  isToListenClicked: boolean;
+  isOwnedAlbumsClicked: boolean;
+  isSoughtAlbumsClicked: boolean;
+
+  songList: UserSongStatus[];
+  albumList: UserAlbumStatus[];
   constructor() { }
 
   ngOnInit() {
   }
 
+  reset() {
+    this.isOwnedAlbumsClicked =
+    this.isSoughtAlbumsClicked =
+    this.isYourSongsClicked =
+    this.isToListenClicked = false;
+  }
+
+  showListenedSongs() {
+
+  }
+
+  showSongsToListen() {
+
+  }
+
+  showOwnedAlbums() {
+
+  }
+
+  showSoughtAlbums() {
+
+  }
 }
