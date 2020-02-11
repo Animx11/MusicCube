a698f3024d80f2cc7de8c35d15ad64456406723c
przand1 <przand1@st.amu.edu.pl>
2020-02-11 13:51:42 +0100

    implement max length test

--- src/main/java/musiccube/advancedsearch/SongByMaxLengthSearchDecorator.java
+++ src/main/java/musiccube/advancedsearch/SongByMaxLengthSearchDecorator.java
@@ -1,12 +1,14 @@
 package musiccube.advancedsearch;
 
 public class SongByMaxLengthSearchDecorator extends AbstractAdvancedSearch {
-    public SongByMaxLengthSearchDecorator(AbstractAdvancedSearch search, String minlength) {
+    private int length;
+    SongByMaxLengthSearchDecorator(AbstractAdvancedSearch search, String minlength) {
         super(search);
+        this.length = Integer.parseInt(minlength);
     }
 
     @Override
     String generateQuery() {
-        return null;
+        return decorated.generateQuery() + "s.songLengthSeconds < " + length;
     }
 }
