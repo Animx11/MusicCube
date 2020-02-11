03f34a3af2f229dff39c3832e06ea9b2d534ff20
przand1 <przand1@st.amu.edu.pl>
2020-02-11 14:16:46 +0100

    apply NameInQueryBuilder in SongByTitleSearch

--- src/main/java/musiccube/advancedsearch/SongByTitleSearchDecorator.java
+++ src/main/java/musiccube/advancedsearch/SongByTitleSearchDecorator.java
@@ -10,21 +10,10 @@ public class SongByTitleSearchDecorator extends AbstractAdvancedSearchWithParams
 
     @Override
     String generateQuery() {
-        String[] titlesArr = paramValues.split(",");
-        StringBuilder query = new StringBuilder("LOWER(s.songName) ");
-        if (negated) {
-            query.append("NOT ");
-        }
-        query.append("IN( ");
-        int len = titlesArr.length;
-        for (int i = 0; i < len; i++) {
-            query.append("LOWER(").append(titlesArr[i]).append(")");
-            if (i < len-1) {
-                query.append(",");
-            }
-            query.append(" ");
-        }
-
-        return decorated.generateQuery() + query.toString();
+        return decorated.generateQuery() + NameInQueryBuilder.build(
+                "s.songName",
+                paramValues,
+                negated
+        );
     }
 }
