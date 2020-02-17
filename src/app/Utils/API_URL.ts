4204b602e4daededb5a584270ae154ecefefbc72
animx11 <animx11@gmail.com>
2020-02-17 16:30:44 +0100

    v5

--- src/app/Utils/API_URL.ts
+++ src/app/Utils/API_URL.ts
@@ -1,5 +1,6 @@
-// export const api_url = "http://192.168.56.101:8080/api";
+export const api_url = "http://150.254.79.34:8080/api";
 
-export const api_url = 'http://localhost:8080/api';
-export const this_url = 'http://localhost:4200/news';
+// export const api_url = 'http://localhost:8080/api';
+export const this_url = 'http://localhost:4200';
+export const this_news_url = 'http://localhost:4200/news';
 
