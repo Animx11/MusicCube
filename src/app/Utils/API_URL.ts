efb696f6cb923d7067fef637149a02374e2be917
animx11 <animx11@gmail.com>
2020-02-17 17:05:06 +0100

    x

--- src/app/Utils/API_URL.ts
+++ src/app/Utils/API_URL.ts
@@ -1,5 +1,7 @@
 // export const api_url = "http://192.168.56.101:8080/api";
 
-export const api_url = 'http://localhost:8080/api';
+export const api_url = 'https://boiling-anchorage-24961.herokuapp.com/api';
+
+//export const api_url = 'http://localhost:8080/api';
 export const this_url = 'http://localhost:4200/';
 
