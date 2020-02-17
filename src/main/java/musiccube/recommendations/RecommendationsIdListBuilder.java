6cf5022621b44a0c6fb2b61d737d0f8f9fe23bbb
przand1 <przand1@st.amu.edu.pl>
2020-02-17 13:00:41 +0100

    fix bug

--- src/main/java/musiccube/recommendations/RecommendationsIdListBuilder.java
+++ src/main/java/musiccube/recommendations/RecommendationsIdListBuilder.java
@@ -2,26 +2,42 @@ package musiccube.recommendations;
 
 import java.util.*;
 
-import static java.util.Comparator.reverseOrder;
-
 public class RecommendationsIdListBuilder {
     private RecommendationsIdListBuilder() {}
 
 
     public static List<Integer> build(List<Set<Integer>> idSets, int limit) {
+        if (limit <= 0) {
+            return new ArrayList<>();
+        }
+
         final HashMap<Integer,Integer> idsAndWeightsMap = new HashMap<>();
-        idSets.forEach(set -> set.forEach(id -> {
-            if (idsAndWeightsMap.containsKey(id)) {
-                idsAndWeightsMap.replace(id,idsAndWeightsMap.get(id) + 1);
-            } else {
-                idsAndWeightsMap.put(id,1);
-            }
-        }));
+
+        processIdSets(idSets, idsAndWeightsMap);
+
+        return getResultList(limit, idsAndWeightsMap);
+    }
+
+    private static List<Integer> getResultList(int limit, HashMap<Integer, Integer> idsAndWeightsMap) {
         List<Integer> result = new ArrayList<>(sortByValue(idsAndWeightsMap).keySet());
         if (result.size() > limit) {
             result = result.subList(0,limit);
         }
         return result;
+    }
+
+    private static void processIdSets(List<Set<Integer>> idSets, HashMap<Integer, Integer> idsAndWeightsMap) {
+        idSets.forEach(set -> set.forEach(id -> {
+            processIdSet(idsAndWeightsMap, id);
+        }));
+    }
+
+    private static void processIdSet(HashMap<Integer, Integer> idsAndWeightsMap, Integer id) {
+        if (idsAndWeightsMap.containsKey(id)) {
+            idsAndWeightsMap.replace(id,idsAndWeightsMap.get(id) + 1);
+        } else {
+            idsAndWeightsMap.put(id,1);
+        }
     }
 
     private static HashMap<Integer, Integer> sortByValue(HashMap<Integer, Integer> hm)
