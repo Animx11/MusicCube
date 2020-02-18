33364dc18676f113c2d850bf90e993d50fe58467
przand1 <przand1@st.amu.edu.pl>
2020-02-18 01:54:44 +0100

    add heroku url

--- src/app/Utils/API_URL.ts
+++ src/app/Utils/API_URL.ts
@@ -4,4 +4,5 @@ export const api_url = 'https://boiling-anchorage-24961.herokuapp.com/api';
 
 //export const api_url = 'http://localhost:8080/api';
 export const this_url = 'http://localhost:4200/';
+//export const this_url = 'https://musiccube.herokuapp.com';
 
